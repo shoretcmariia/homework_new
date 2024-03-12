f0fbf32f273e7d2e636e9e96cb13e1540b876efa
maria shorets <0@MacBook-Pro-Maria.local>
2024-03-13 00:19:10 +0300

    убрала ошибку

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -8,29 +8,21 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        int m = 2;
-        int n = 3;
-
-        int result = Ackermann(m, n);
-        Console.WriteLine($"A({m}, {n}) = {result}");
-    }
-
     static int Ackermann(int m, int n)
     {
         if (m == 0)
-        {
             return n + 1;
-        }
         else if (m > 0 && n == 0)
-        {
             return Ackermann(m - 1, 1);
-        }
         else
-        {
             return Ackermann(m - 1, Ackermann(m, n - 1));
-        }
+    }
+
+    static void Main(string[] args)
+    {
+        int m = 2, n = 3;
+        int result = Ackermann(m, n);
+        Console.WriteLine($"A({m}, {n}) = {result}");
     }
 }
 
