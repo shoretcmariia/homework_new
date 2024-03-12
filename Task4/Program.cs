70b22fa9cc1eda37f5b57f540ad69fc0363e6688
maria shorets <0@MacBook-Pro-Maria.local>
2024-03-13 00:27:10 +0300

    попытка исправить ошибку

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -12,7 +12,7 @@ class Program
     {
         if (m == 0)
             return n + 1;
-        else if (m > 0 && n == 0)
+        else if (n == 0)
             return Ackermann(m - 1, 1);
         else
             return Ackermann(m - 1, Ackermann(m, n - 1));
@@ -22,7 +22,6 @@ class Program
     {
         int m = 2, n = 3;
         int result = Ackermann(m, n);
-        Console.WriteLine($"A({m}, {n}) = {result}");
+        Console.WriteLine($"m = {m}, n = {n} -> A({m},{n}) = {result}");
     }
 }
-
