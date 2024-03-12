c600064bb162827d761c0074bb40b08ef0245e37
maria shorets <0@MacBook-Pro-Maria.local>
2024-03-13 00:33:37 +0300

    добавила третью задачу

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте произвольный массив на C#. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+//[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write($"{array[index]} ");
+            PrintArrayReversed(array, index - 1);
+        }
+    }
+}
