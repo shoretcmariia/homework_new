0c764575ecb8027ac1427c817928cdc1bd47243a
maria shorets <0@MacBook-Pro-Maria.local>
2024-03-13 00:09:18 +0300

    програм

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -13,21 +13,55 @@ class Program
 {
     static void Main()
     {
-        // Задаем значения M и N
+        
         int M = 4;
         int N = 8; 
 
-        // Выводим натуральные числа от M до N
+        
         PrintNumbers(M, N);
     }
 
-    // Рекурсивная функция для вывода натуральных чисел от M до N
+  
     static void PrintNumbers(int M, int N)
     {
         if (M <= N)
         {
             Console.Write($"{M} ");
-            PrintNumbers(M + 1, N); // Рекурсивный вызов для следующего числа
+            PrintNumbers(M + 1, N); 
+        }
+    }
+}
+
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+
+
+//Console.WriteLine("Hello World");
+//Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        int M = 1;
+        int N = 5;
+
+        
+        PrintNumbers(M, N);
+    }
+
+    
+    static void PrintNumbers(int M, int N)
+    {
+        if (M <= N)
+        {
+            Console.Write($"{M} ");
+            PrintNumbers(M + 1, N); 
         }
     }
 }
