fa11c152b9d6a946330c0b76f147ff05652f7514
maria shorets <0@MacBook-Pro-Maria.local>
2024-03-12 20:11:46 +0300

    первый тест

---
+++ homework/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Console.WriteLine("Hello World");
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем значения M и N
+        int M = 1;
+        int N = 5;
+
+        // Выводим натуральные числа от M до N
+        PrintNumbers(M, N);
+    }
+
+    // Рекурсивная функция для вывода натуральных чисел от M до N
+    static void PrintNumbers(int M, int N)
+    {
+        if (M <= N)
+        {
+            Console.Write($"{M} ");
+            PrintNumbers(M + 1, N); // Рекурсивный вызов для следующего числа
+        }
+    }
+}
+
